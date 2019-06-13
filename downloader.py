23fc09b8723e94876198fee1af1bf4edf5643a2a
Jagan Prem <notjagan@gmail.com>
2019-06-13 14:17:47 -0400

    Add match footage downloading script

---
+++ downloader.py
@@ -0,0 +1,93 @@
+import json
+import random
+import os
+from glob import glob
+from datetime import datetime
+
+import requests
+import pytube
+from colors import color
+
+with open("tba.json", "r") as auth:
+    headers = json.load(auth)
+api_url = 'https://www.thebluealliance.com/api/v3'
+
+response = requests.get('{}/status'.format(api_url), headers=headers)
+if response.status_code != 200:
+    print(color("The Blue Alliance API is currently down. " \
+                "Please try again later.", fg='red'))
+    raise SystemExit
+
+while True:
+    print()
+
+    event_key = input("Event key: ")
+    response = requests.get('{}/event/{}'.format(api_url, event_key),
+                            headers=headers)
+    if response.status_code != 200:
+        print(color("Event key invalid.", fg='red'))
+        continue
+    body = json.loads(response.text)
+    year = body['year']
+    event_code = body['event_code']
+
+    response = requests.get('{}/event/{}/matches'.format(api_url, event_key),
+                            headers=headers)
+    assert response.status_code == 200
+    all_matches = json.loads(response.text)
+    matches = [match for match in all_matches
+               if any(video['type'] == 'youtube' for video in match['videos'])]
+
+    total = len(matches)
+    if total == 0:
+        print(color("No matches with video found.", fg='red'))
+        continue
+    print(color("{} match(es) with video loaded ({} total)."
+                .format(total, len(all_matches)), fg='green'))
+
+    while True:
+        number = input("Number of matches (blank to download all): ")
+        if number == '':
+            n = total
+            break
+        elif number.isdigit():
+            n = int(number)
+            if n <= 0:
+                print(color("Number of matches must be positive.", fg='red'))
+                continue
+            elif n > total:
+                n = total
+            break
+        else:
+            print(color("Invalid number.", fg='red'))
+
+    path = os.path.join("data", "videos", str(year), event_code)
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    print("Downloading {} match(es).".format(n))
+    random.shuffle(matches)
+    i = 0
+    index = -1
+    while i < n:
+        index += 1
+        match = matches[index]
+        key = match['key']
+        if any(glob(os.path.join(path, key) + "*")):
+            continue
+        print("Current match: {}".format(key))
+
+        videos = match['videos']
+        video = [v for v in videos if v['type'] == 'youtube'][0]
+        url = pytube.extract.watch_url(video['key'])
+        print("YouTube URL: {}".format(url))
+
+        print("Downloading...")
+        start = datetime.now()
+        yt = pytube.YouTube(url)
+        yt.streams.first().download(output_path=path, filename=key)
+        elapsed = datetime.now() - start
+        print(color("Download complete. ", fg='green') +
+              "({} elapsed)".format(elapsed))
+
+        i += 1
