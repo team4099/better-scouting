99b2df0b02a5ee57629427b27f911681097f5fd2
Jagan Prem <notjagan@gmail.com>
2019-06-13 14:18:28 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='better-scouting',
+      version='0.0.1',
+      description='Scouting, but better.',
+      author='Jagan Prem',
+      author_email='notjagan@gmail.com',
+      install_requires=[
+        'requests',
+        'ansicolors',
+        'pytube',
+      ])
